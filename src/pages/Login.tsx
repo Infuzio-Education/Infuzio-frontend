e13506eb2a23287622cff811d7f93e300e73d427
rithasahmed12 <ahmedrithas48@gmail.com>
2024-09-17 16:51:50 +0530

    login feature done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ArrowRight } from 'lucide-react';
 import { useFormik } from "formik";
 import { LoginValidationSchema } from '../validations/LoginValidationSchema';
