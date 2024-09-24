4e5215d1e296c200ea01b3b9f4ac0aeb7f00fe40
Swalih pk <nisunasih135@gmail.com>
2024-09-24 09:11:31 +0530

    confict fixed on axios

--- src/api/superAdmin.ts
+++ src/api/superAdmin.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 
 export const superLogin = async(body:string)=>{
     try {
-        const response = await Api.post(superAdminEndpoints.login,body);
+        const response = await Api.post(superAdminEndpoints.login, body);
         return response;
     } catch (error) {
         if (axios.isAxiosError(error)) {
@@ -16,3 +16,32 @@ export const superLogin = async(body:string)=>{
         }
     }
 }
+
+export const getSyllabus = async () => {
+    try {
+        const response = await Api.get(superAdminEndpoints.syllabus);
+        return response;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            return error.response;
+        } else {
+            console.error('Unexpected error:', error);
+            throw error;
+        }
+    }
+}
+
+export const createSchool = async () => {
+    try {
+        const response = await Api.get(superAdminEndpoints.createSchool);
+        return response;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            return error.response;
+        } else {
+            console.error('Unexpected error:', error);
+            throw error;
+        }
+    }
+}
+
--- src/endpoints/superAdmin.ts
+++ src/endpoints/superAdmin.ts
@@ -1,7 +1,8 @@
 
 const superAdminEndpoints = {
-    login:'superAdmin/login',
-
+    login: 'superAdmin/login',
+    syllabus: 'superAdmin/syllabus',
+    createSchool: 'superAdmin/school'
 }
 
 export default superAdminEndpoints;
