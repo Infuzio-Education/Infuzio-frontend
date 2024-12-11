182c6d8e630cf35ed85a3618bf4146c2f91c3276
Swalih pk <nisunasih135@gmail.com>
2024-12-11 20:06:58 +0530

    Exams and staff profile  designed

--- src/routes/StaffRoutes.tsx
+++ src/routes/StaffRoutes.tsx
@@ -7,6 +7,7 @@ import Announcements from "../pages/staffs/Announcements";
 import HomeWorkouts from "../pages/staffs/HomeWorkouts";
 import UnitTests from "../pages/staffs/UnitTests";
 import Exams from "../pages/staffs/Exams";
+import StaffProfile from "../pages/staffs/StaffProfile";
 
 const StaffRoutes = () => {
     return (
@@ -20,6 +21,7 @@ const StaffRoutes = () => {
                 <Route path="home-workouts" element={<HomeWorkouts />} />
                 <Route path="unit-tests" element={<UnitTests />} />
                 <Route path="exams" element={<Exams />} />
+                <Route path="profile" element={<StaffProfile />} />
             </Route>
         </Routes>
     );
